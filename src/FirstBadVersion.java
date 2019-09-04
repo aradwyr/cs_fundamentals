9cd688ade8a361fdcd065607a2899ff69958df62
Sarah Dwyer <dwyersarahb@gmail.com>
2019-09-04 12:55:08 -0700

    Changes to background info

--- src/FirstBadVersion.java
+++ src/FirstBadVersion.java
@@ -1,10 +1,23 @@
 /**
+ * First Bad Version
  *
- * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version
+ * of your product fails the quality check. Since each version is developed based on the previous version, all the
+ * versions after a bad version are also bad.
  *
- * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following
+ * ones to be bad.
  *
- * You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ * You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to
+ * find the first bad version. You should minimize the number of calls to the API.
+ *
+ * isBadVersion function:
+ * Given n = 5, and version = 4 is the first bad version.
+ *
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ *
  *
  * Time: O(logn), Space: O(1)
  */
